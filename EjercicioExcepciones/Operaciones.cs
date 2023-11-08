5a01bcb6827e357e16ab2d31cd9ebf2a7bedc6ad
Axl-Gomez-2909 <148070790+Axl-Gomez-2909@users.noreply.github.com>
2023-11-07 21:17:35 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioExcepciones/Operaciones.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExcepciones
+{
+    public class Operaciones
+    {
+        //Metodo Sumar 
+        public double Sumar(double a, double b)
+        {
+            
+            return a + b;
+        }
+        //Metodo Restar
+        public double Restar(double a, double b)
+        {
+            return a - b;
+        }
+        //Metodo Multiplicar
+        public double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+        //Metodo Dividir 
+        public double Dividir(double a, double b)
+        {
+            //Validación para no dividir entre cero
+            if (b == 0)
+            {
+                //Asignación a la exepción DivideByZeroException un mensaje por si el usuario ingresa cer0
+                throw new DivideByZeroException("No es posible dividir entre cero.");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ EjercicioExcepciones/Program.cs
@@ -0,0 +1,62 @@
+﻿using EjercicioExcepciones;
+//Instanciamiento de la clase operaciones
+Operaciones operaciones = new Operaciones();
+
+//Declaración de variables
+double num1=0, num2=0;
+//Declaración de variable booleana para validar la entrada en los ciclos
+bool entradaValida = false;
+
+//Ciclo para validar si se comete un catch por error de formato 
+do
+{
+    //Validación si el primer número no sea una letra o cual quier cosa
+    try
+    {
+        Console.Write("Ingrese el primer número: ");
+        num1 = double.Parse(Console.ReadLine());
+        entradaValida = true;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Entrada inválida. Ingrese un número válido.");
+    }
+} while (!entradaValida);
+
+entradaValida = false;
+
+//Ciclo para validar si se comete un catch por error de formato 
+do
+{
+    //Validación si el segundo número no sea una letra o cual quier cosa
+    try
+    {
+        Console.Write("Ingrese el segundo número: ");
+        num2 = double.Parse(Console.ReadLine());
+        entradaValida = true;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Entrada inválida. Ingrese un número válido.");
+    }
+} while (!entradaValida);
+
+//Validación de dividir entre cero
+try
+{
+    double suma = operaciones.Sumar(num1, num2);
+    double resta = operaciones.Restar(num1, num2);
+    double multiplicacion = operaciones.Multiplicar(num1, num2);
+    double division = operaciones.Dividir(num1, num2);
+
+    Console.WriteLine($"Resultado de la suma: {num1}+{num2}= {suma}");
+    Console.WriteLine($"Resultado de la resta:  {num1}-{num2}= {resta}");
+    Console.WriteLine($"Resultado de la multiplicación:  {num1}*{num2}= {multiplicacion}");
+    Console.WriteLine($"Resultado de la división:  {num1}/{num2}= {division}");
+}
+catch (DivideByZeroException ex)
+{
+
+    Console.WriteLine("Error fatal: " + ex.Message);
+}
+    
